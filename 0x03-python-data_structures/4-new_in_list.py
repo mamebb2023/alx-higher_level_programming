c3d0a611de98ae853fd17a3b4fcb6f7a2dc7982f
Mohammednur <mamebb2023@gmail.com>
2023-03-14 20:09:35 +0300

    Replace an element in the new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return new_list
+
+    new_list = [i for i in my_list]
+    new_list[idx] = element
+    return new_list
