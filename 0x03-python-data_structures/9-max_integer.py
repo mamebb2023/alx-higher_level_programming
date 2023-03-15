605e688285cb802ffe80dec25065b4085af43fc0
Mohammednur <mamebb2023@gmail.com>
2023-03-15 20:16:13 +0300

    Maximum integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    val = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > val:
+            val = my_list[i]
+    return val
