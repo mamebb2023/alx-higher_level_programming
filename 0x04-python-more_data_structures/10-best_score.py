1c78741bef640dde4b2b2fd96977b9afda54f9c0
Mohammednur <mamebb2023@gmail.com>
2023-03-17 09:46:01 +0300

    Print the hightest score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    d = a_dictionary
+    if not isinstance(d, dict) or len(d) == 0:
+        return None
+    
+    key = list(d.keys())[0]
+    val = d[key]
+
+    for k, v in d.items():
+        if v > val:
+            val = v
+            key = k
+
+    return (key)
