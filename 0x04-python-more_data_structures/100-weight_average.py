dda275dcfbeb3f52e3df9666145ce84b2ff7f7d6
Mohammednur <mamebb2023@gmail.com>
2023-03-17 18:00:10 +0300

    Print the weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not ininstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
+    ava = 0
+    size = 0
+    for x in my_list:
+        ava += (x[0] * x[1])
+        size += x[1]
+    return (ava / size)
