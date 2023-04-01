5fff7024e4dc5195fab6d69ffcea4ffed6ab3fd2
Mohammednur <mamebb2023@gmail.com>
2023-04-01 11:45:29 +0300

    Square and position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,11 +44,11 @@ class Square:
         position (int): position of the square
     """
     @position.setter
-    def position(self, value):
+     def position(self, value):
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or 
-            not all(num >= 0 for num in value)):
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
