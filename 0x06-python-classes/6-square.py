77ffb045c4e6a07c4f98166787ca4d92b7d5e4e6
Mohammednur <mamebb2023@gmail.com>
2023-04-01 11:35:00 +0300

    Square and position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,7 +26,10 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+
+        [print("") for i in range(self.__position[1])]
         for i in range(0, self.__size):
+            [print(" ", end="") for x in range(self.__position[0])
             [print("#", end="") for j in range(self.__size)]
             print("")
 
