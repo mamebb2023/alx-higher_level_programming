6c05b011fe8f8525952e41fa01471499fb45a2b8
Mohammednur <mamebb2023@gmail.com>
2023-04-03 21:56:54 +0300

    Print the rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         """
         if self.__height == 0 or self.__width == 0:
             return ("")
-        
+
         x = []
         for i in range(self.__height):
             [x.append("#") for j in range(self.__width)]
