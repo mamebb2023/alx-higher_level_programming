259d7f539e7cff480d07d84be66b7e02dd9ca734
Mohammednur <mamebb2023@gmail.com>
2023-04-12 09:13:18 +0300

    My List

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Creating a class that inherits from list """
+
+
+class Mylist(list):
+    """ A class that inherites form list """
+
+    def print_sorted(self):
+        """ Prints the list of ints in asending sort """
+        print(sorted(list))
