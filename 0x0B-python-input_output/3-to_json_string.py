9e2b90d77b2531cc4a8f24a7eec0e48a7c3f234a
Mohammednur <mamebb2023@gmail.com>
2023-04-13 15:03:06 +0300

    Return the JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" JSON representation """
+import json
+
+def to_json_string(my_obj):
+    """ Returns the json 
+    representation of a my_obj """
+    return json.dumps(my_obj)
