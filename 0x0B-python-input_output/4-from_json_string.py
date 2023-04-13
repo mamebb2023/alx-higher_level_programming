891a5c4065347331cf1e92a28466d2597364c159
Mohammednur <mamebb2023@gmail.com>
2023-04-13 15:07:44 +0300

    To python data structure

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" JSON representation """
+import json
+
+def from_json_string(my_str):
+    """ Returns the python data structure
+    from json representation """
+    return json.loads(my_str)
