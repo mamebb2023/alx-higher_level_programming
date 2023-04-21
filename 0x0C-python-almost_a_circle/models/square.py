48afd5b681c8c66a27b8e4e005d384e34510b0c5
Mohammednur <mamebb2023@gmail.com>
2023-04-21 10:32:28 +0300

    The Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,16 +7,20 @@ class Square(Rectangle):
     """ Defines a square """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Properties for a new square 
+        """ Properties for a new square
 
         Args:
             size (int): the size of the square
             x (int): the x position of the square
             y (int): the y postiton of the square
-            id (any): a unique id for the square
+            id (int): a unique id for the square
         """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ Prints a string for the square """
-        print("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size))
+        print("[Square] ({}) {}/{} - {}".format(
+                                        self.id, 
+                                        self.x, 
+                                        self.y, 
+                                        self.size))
