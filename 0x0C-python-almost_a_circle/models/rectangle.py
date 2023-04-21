d2b21497a4e56e74e790a04b4353d76ca223106a
Mohammednur <mamebb2023@gmail.com>
2023-04-21 10:58:18 +0300

    Square dict

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,3 +120,12 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                     self.x, self.y,
                                                     self.width, self.height)
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
