29d50dc162f92c786810957a389e9e441e6b1fb0
Mohammednur <mamebb2023@gmail.com>
2023-04-21 10:45:50 +0300

    The Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,16 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    """ Getter/Setter methods for the square """
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ Prints a string for the square """
         return ("[Square] ({}) {}/{} - {}".format(
