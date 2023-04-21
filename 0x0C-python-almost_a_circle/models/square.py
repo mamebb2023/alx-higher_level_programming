c7c459c83ee0d5d8805ff256182a2522e76392bf
Mohammednur <mamebb2023@gmail.com>
2023-04-21 10:50:45 +0300

    The Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,7 +46,7 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-     def to_dictionary(self):
+    def to_dictionary(self):
         """ Returns the dictionary version of self """
         return dict(id=self.id, size=self.size, x=self.x, y=self.y)
 
