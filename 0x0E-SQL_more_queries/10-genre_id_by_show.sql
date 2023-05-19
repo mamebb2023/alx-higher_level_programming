93e3eaefe868abff1f55aa18ab4483e081fbb3ad
Mohammednur <mamebb2023@gmail.com>
2023-05-19 14:58:00 +0300

    List all

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all shows in hbtn_0d_tvshows
-SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         INNER JOIN `tv_show_genres` AS g
 	ON s.`id` = g.`show_id`
