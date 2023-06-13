1763c5e8c9185311c808ee285240232c59a1dafe
Mohammednur <mamebb2023@gmail.com>
2023-06-13 19:29:28 +0300

    JavaScript factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (n === NaN || n === 0) return 1;
-  return n * factorial(n - 1);
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-const n = process.argv[2];
-console.log(factorial(Number(n)));
+console.log(factorial(Number(process.argv[2])));
