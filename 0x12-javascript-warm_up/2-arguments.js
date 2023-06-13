7feab9b1ac7d9feeae4f2c279cc5a6f5a3a2e362
Mohammednur <mamebb2023@gmail.com>
2023-06-13 17:32:03 +0300

    JavaScript arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const n = process.argv.length;
-console.log(n === 2 ? 'No Argument' : n === 3 ? 'Argument found' : 'Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
