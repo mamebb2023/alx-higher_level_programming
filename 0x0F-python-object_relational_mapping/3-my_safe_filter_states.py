936bd15e1528a849cf280c6f15c648b65dc9c912
Mohammednur <mamebb2023@gmail.com>
2023-06-17 19:47:52 +0300

    MySQL filter and others

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Filter states from the user's input
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
