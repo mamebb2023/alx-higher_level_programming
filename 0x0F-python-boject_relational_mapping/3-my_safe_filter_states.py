b50c310a38dc609ad1a773a8067e293773336a24
Mohammednur <mamebb2023@gmail.com>
2023-06-17 18:51:09 +0300

    My safe filter states

--- 0x0F-python-boject_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-boject_relational_mapping/3-my_safe_filter_states.py
@@ -4,8 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    fil = sys.argv[4]
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[] == sys.argv[4]]
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
