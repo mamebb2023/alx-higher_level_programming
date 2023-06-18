ea1d91bdced79ad4319dc59eb281fc99e0736f57
Mohammednur <mamebb2023@gmail.com>
2023-06-18 10:41:30 +0300

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" List all states form a database """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states \
+                WHERE name REGEXP '^N' \
+                ORDER BY id ASC")
+    [print(row) for row in c.fetchall()]
+    c.close()
+    db.close()
