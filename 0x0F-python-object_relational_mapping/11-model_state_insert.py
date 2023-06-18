2cf5442a0a6f072a2dde9135121a7eb5898473bb
Mohammednur <mamebb2023@gmail.com>
2023-06-18 11:38:58 +0300

    Model state insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script that prints the first State object from the database """
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    estado = State()
+    estado.name = 'Louisiana'
+    session.add(estado)
+    session.commit()
+    print(estado.id)
+    session.close()
