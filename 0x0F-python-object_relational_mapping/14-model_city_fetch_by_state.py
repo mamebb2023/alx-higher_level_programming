4d326e9e2a46478201e158600c2b6e8321670ff2
Mohammednur <mamebb2023@gmail.com>
2023-06-18 11:42:43 +0300

    Model city fetch by state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script that prints the first State object from the database """
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from model_city import Base, City
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city in session.query(City).join(State).order_by(City.id).all():
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    session.close()
