47df2c18fdcfca921c3340737f82ae90553ff743
Mohammednur <mamebb2023@gmail.com>
2023-06-18 11:36:55 +0300

    Model state that contain a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script that prints the first State object from the database """
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).filter(State.name.like('%a%'))
+    for sts in first_state:
+        print("{}: {}".format(sts.id, sts.name))
+    session.close()
