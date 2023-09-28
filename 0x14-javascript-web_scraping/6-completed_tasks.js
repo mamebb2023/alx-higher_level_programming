c4ba2af4d617f636cf5edc79e6776f55b7381278
Mohammednur <mamebb2023@gmail.com>
2023-09-28 19:38:45 +0300

    update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,9 +8,8 @@ request(process.argv[2], function (error, response, body) {
       if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
       } else if (todo.completed) {
-        completed[todo.userId] += 1;
+       completed[todo.userId] += 1;
       }
     });
     console.log(completed);
-  }
-);
+  });
